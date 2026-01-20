1a24dc3bb8de33f023e269d9d1ecc0f177719e58
CarlosLopez <131546244+lordPobre@users.noreply.github.com>
2026-01-20 17:56:02 -0300

    Actualizar urls.py

--- config/urls.py
+++ config/urls.py
@@ -6,14 +6,9 @@ from django.contrib.auth import views as auth_views
 from apps.asistencia.views import ServiceWorkerView
 
 urlpatterns = [
-    # 1. TU LOGIN PERSONALIZADO (Va primero para ganar prioridad)
     path('accounts/login/', auth_views.LoginView.as_view(template_name='asistencia/login.html'), name='login'),
-
-    # 2. TU LOGOUT
     path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
     path('sw.js', ServiceWorkerView.as_view(), name='sw_js'),
-
-    # 3. ADMIN Y EL RESTO DE LA APP
     path('admin/', admin.site.urls),
     path('', include('apps.asistencia.urls')),
 ]
